5ea21c15a692df3e1e19d47e88fe7b6a97606931
Taeil.Eom <tale0312@gmail.com>
2022-08-25 00:55:08 +0900

    useEffect syntax

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,11 +2,38 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function hiFn(){
+    console.log("created :)");
+    return byeFn;
+  }
+  function byeFn(){
+    console.log("bye :(")
+  }
+
+  useEffect(hiFn,[]);
+  return <h1> Hello </h1>;
+
+  // useEffect(() => {
+  //   console.log("hi :)");
+  //   return () => console.log("bye :(")
+  // },[]);
+
+  // useEffect(function () {
+  //   console.log("hi :)");
+  //   return function() {
+  //     console.log("bye :(")
+  //   }
+  // },[])
+}
+
 function App() {
   const [counter, setCounter] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setCounter((prev) => prev+1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClickHide = () => setShowing((prev) => !prev);
 
   console.log("I run all the time.");
   // const iRunOnlyOnce = () => {
@@ -38,6 +65,8 @@ function App() {
         type="text"
         placeholder="Search keyword"
       />
+      {showing ? <Hello /> : null}
+      <button onClick={onClickHide}>{showing ? "Hide":"Show"}</button>
     </div>
   );
 }
