18825fe4e847142d674a800d505f019351cc97d9
tale <tale0312@gmail.com>
2022-08-26 15:11:58 +0900

    api mapping

--- react_movie_app/src/App.js
+++ react_movie_app/src/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  // Todo List //
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value); 
@@ -15,6 +16,29 @@ function App() {
   }
   // console.log(toDo);
   // console.log(toDos);
+
+  // Object Tracker //
+  const [loading, setLoading] = useState(true);
+  
+  const [coins, setCoins] = useState([]);
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    // fetch("https:api.coinpaprika.com/v1/tickers")
+    //   .then(response => response.json())
+    //     .then((json) => {
+    //       console.log(json);
+    //       setCoins(json);
+    //       setLoading(false);
+    //     });
+    fetch('https://api.sampleapis.com/coffee/hot')
+      .then(response => response.json())
+        .then((json) => {
+          console.log(json);
+          setCoffees(json);
+          setLoading(false);
+          }
+        );
+  },[])
   return (
     <div>
       <h1>Added List ({toDos.length})</h1>
@@ -33,7 +57,37 @@ function App() {
           <li key={index}>{item}</li>
         ))}
       </ul>
+      <div>
+        <h1>The Coffees ({coffees.length})</h1>
+        {loading ? <strong>Loading... </strong>:null}
+        <ul>
+          {coffees.map((coffee) => 
+            <li>
+              <h1>{coffee.title}</h1> 
+              <h4>{coffee.description}</h4>
+              <h5>{coffee.image}</h5>
+              <img src={coffee.image} width="100" height="132"></img>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div>
+        <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? (
+        <strong>Loading...</strong>
+        ) : (
+          <select>
+            {coins.map((coin) => (
+              <option>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+              </option>
+            ))};
+          </select>
+        )}
+        
+      </div>
     </div>
+    
   );
 }
 
