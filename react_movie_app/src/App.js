8dc37f2cc003db98ccce5bc98008195dc9264db9
tale <tale0312@gmail.com>
2022-08-26 13:14:01 +0900

    .map

--- react_movie_app/src/App.js
+++ react_movie_app/src/App.js
@@ -14,16 +14,27 @@ function App() {
     setToDos(currentArray => [toDo, ...currentArray]);
   }
   // console.log(toDo);
-  console.log(toDos);
+  // console.log(toDos);
   return (
     <div>
       <h1>Added List ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-        <input value={toDo || ''} type="text" onChange={onChange} placeholder="write your todo..." />
+        <input
+          value={toDo || ""}
+          type="text"
+          onChange={onChange}
+          placeholder="write your todo..."
+        />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
