52387935f12db00cdfae2d8b71d4f22c2778e241
tale <tale0312@gmail.com>
2022-08-26 15:57:43 +0900

    movie api

--- react_movie_app/src/App.js
+++ react_movie_app/src/App.js
@@ -1,94 +1,49 @@
 import { useEffect, useState } from "react";
-
 function App() {
-  // Todo List //
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value); 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // console.log(toDo);
-    if (toDo ==="") {
-      return;
-    } 
-    setToDo("");
-    setToDos(currentArray => [toDo, ...currentArray]);
+  const [loading,setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )).json();
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    // );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   }
-  // console.log(toDo);
-  // console.log(toDos);
-
-  // Object Tracker //
-  const [loading, setLoading] = useState(true);
-  
-  const [coins, setCoins] = useState([]);
-  const [coffees, setCoffees] = useState([]);
   useEffect(() => {
-    // fetch("https:api.coinpaprika.com/v1/tickers")
-    //   .then(response => response.json())
-    //     .then((json) => {
-    //       console.log(json);
-    //       setCoins(json);
-    //       setLoading(false);
-    //     });
-    fetch('https://api.sampleapis.com/coffee/hot')
-      .then(response => response.json())
-        .then((json) => {
-          console.log(json);
-          setCoffees(json);
-          setLoading(false);
-          }
-        );
-  },[])
-  return (
-    <div>
-      <h1>Added List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo || ""}
-          type="text"
-          onChange={onChange}
-          placeholder="write your todo..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+    getMovies();
+  }, []);
+  // useEffect(() => {
+  //   fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+  //     .then(response => response.json())
+  //       .then((json) => {
+  //         setMovies(json.data.movies);
+  //         setLoading(false);
+  //       });
+  // }, [])
+  console.log(movies);
+  return <div>
+    {loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.generes.map((g,idx) => (
+                <li key={g}>{g}</li>)
+              )};
+            </ul>
+          </div>
         ))}
-      </ul>
-      <div>
-        <h1>The Coffees ({coffees.length})</h1>
-        {loading ? <strong>Loading... </strong>:null}
-        <ul>
-          {coffees.map((coffee) => 
-            <li>
-              <h1>{coffee.title}</h1> 
-              <h4>{coffee.description}</h4>
-              <h5>{coffee.image}</h5>
-              <img src={coffee.image} width="100" height="132"></img>
-            </li>
-          )}
-        </ul>
       </div>
-      <div>
-        <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
-        {loading ? (
-        <strong>Loading...</strong>
-        ) : (
-          <select>
-            {coins.map((coin) => (
-              <option>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-              </option>
-            ))};
-          </select>
-        )}
-        
-      </div>
-    </div>
-    
-  );
+    )};
+  </div>
 }
 
 export default App;
---
+++ react_movie_app/src/App_todo_api.js
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  // Todo List //
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value); 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+    if (toDo ==="") {
+      return;
+    } 
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  }
+  // console.log(toDo);
+  // console.log(toDos);
+
+  // Object Tracker //
+  const [loading, setLoading] = useState(true);
+  
+  const [coins, setCoins] = useState([]);
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    // fetch("https:api.coinpaprika.com/v1/tickers")
+    //   .then(response => response.json())
+    //     .then((json) => {
+    //       console.log(json);
+    //       setCoins(json);
+    //       setLoading(false);
+    //     });
+    fetch('https://api.sampleapis.com/coffee/hot')
+      .then(response => response.json())
+        .then((json) => {
+          console.log(json);
+          setCoffees(json);
+          setLoading(false);
+          }
+        );
+  },[])
+  return (
+    <div>
+      <h1>Added List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo || ""}
+          type="text"
+          onChange={onChange}
+          placeholder="write your todo..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <div>
+        <h1>The Coffees {loading ? "" : `(${coffees.length})`}</h1>
+        {loading ? <strong>Loading... </strong>:null}
+        <ul>
+          {coffees.map((coffee) => 
+            <li>
+              <h1>{coffee.title}</h1> 
+              <h4>{coffee.description}</h4>
+              <h5>{coffee.image}</h5>
+              <img src={coffee.image} width="100" height="132"></img>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div>
+        <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? (
+        <strong>Loading...</strong>
+        ) : (
+          <select>
+            {coins.map((coin) => (
+              <option>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+              </option>
+            ))};
+          </select>
+        )}
+        
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
