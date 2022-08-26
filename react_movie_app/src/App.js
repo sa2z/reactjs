220006b1c21705ff65aefbc06453d1810df059c8
tale <tale0312@gmail.com>
2022-08-26 11:45:19 +0900

    todo input

---
+++ react_movie_app/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value); 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+    if (toDo ==="") {
+      return;
+    } 
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  }
+  // console.log(toDo);
+  console.log(toDos);
+  return (
+    <div>
+      <h1>Added List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo || ''} type="text" onChange={onChange} placeholder="write your todo..." />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  )
+}
+
+export default App;
