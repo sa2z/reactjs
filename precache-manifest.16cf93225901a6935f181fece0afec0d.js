bf67dbc03e766442384d9bb584b471d1244f0240
Taeil.Eom <tale0312@gmail.com>
2022-08-29 23:32:07 +0900

    Updates

---
+++ precache-manifest.16cf93225901a6935f181fece0afec0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a74829bc956b6c202f8",
+    "url": "/static/css/main.2dc89dac.chunk.css"
+  },
+  {
+    "revision": "8a74829bc956b6c202f8",
+    "url": "/static/js/main.8a74829b.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "4073ce42da5a4beeaede",
+    "url": "/static/js/2.4073ce42.chunk.js"
+  },
+  {
+    "revision": "a850c70773011bd5f111",
+    "url": "/static/js/runtime~main.a850c707.js"
+  },
+  {
+    "revision": "1673fd9fc50005c205cc2534423e263c",
+    "url": "/index.html"
+  }
+];
